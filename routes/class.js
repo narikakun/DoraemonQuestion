9d5d9458f6c5370ad3202645e04fc71a2925695d
なりかくん <narikakun@gmail.com>
2023-03-14 20:11:46 +0900

    a

--- routes/class.js
+++ routes/class.js
@@ -1,9 +1,40 @@
 const router = require("express").Router();
-const sqlite3 = require("sqlite3");
-const classData = new sqlite3.Database("./db/database.db");
 
-router.get("/authClass", (req, res) => {
-    res.send("/user/");
+const { MongoClient } = require('mongodb');
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
+
+router.post('/authClass', async function(req, res) {
+    const classId = req.body.classId;
+    console.log(res.app.locals);
+    const collection = res.app.locals.db.collection("classList");
+    const classObj = await collection.findOne({ classId : classId});
+    if (classObj) {
+        res.status(200).json({
+            msg: "クラスが見つかりました。"
+        });
+    } else {
+        res.status(401).json({
+            msg: "クラスIDが見つかりません。"
+        });
+    }
+});
+
+router.post('/createClass', async function(req, res) {
+    const classId = req.body.classId;
+
+    const collection = res.app.locals.db.collection("classList");
+    const classObj = await collection.findOne({ classId : classId});
+    if (classObj) {
+        res.status(200).json({
+            msg: "クラスが見つかりました。"
+        });
+    } else {
+        res.status(401).json({
+            msg: "クラスIDが見つかりません。"
+        });
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,35 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const log4js = require('log4js')
 const logger = log4js.getLogger("default");
 logger.level = 'ALL';
+const bodyParser = require('body-parser');
+const {MongoClient} = require("mongodb");
 
 const server = app.listen(3000, function(){
     logger.info(`Expressで起動しました。 ポート番号: ${server.address().port}`);
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(log4js.connectLogger(log4js.getLogger('express'), {}));
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World");
 });
 
+connectDB();
+app.use("/api", require("./routes/class.js"));
 app.use(express.static('public'));
 
-app.use("api", require("./routes/class.js"));
+async function connectDB () {
+    let dbClient;
+    try {
+        dbClient = await MongoClient.connect(process.env.MongoDB);
+        app.locals.db = await dbClient.db("dQuestion");
+    } catch (err) {
+        console.log(err);
+    } finally {
+        if (dbClient) await dbClient.close();
+    }
+}
