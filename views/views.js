3be2fc5c782ec3708a0be9cfc0ae67847e2b16b0
nrkycn <narikakun@gmail.com>
2023-03-16 11:51:56 +0900

    ディレクトリの移動、ボードリストの追加ˆ

--- views/views.js
+++ views/views.js
@@ -3,14 +3,21 @@ const router = require("express").Router();
 router.get('/', function(req, res) {
     res.render('pages/index');
 });
+
+// ログイン関係
 router.get('/tCreate', function(req, res) {
-    res.render('pages/tCreate');
+    res.render('pages/login/tCreate');
 });
 router.get('/login', function(req, res) {
-    res.render('pages/login');
+    res.render('pages/login/login');
 });
 router.get('/tLogin', function(req, res) {
-    res.render('pages/tLogin');
+    res.render('pages/login/tLogin');
+});
+
+// クラス関係（ログイン後）
+router.get('/class/:classId', function(req, res) {
+    res.render('pages/class/boardList', { classId: req.params.classId });
 });
 
 module.exports = router;
