3ce18da17e6d98199c618c30aca265d1145eac48
なりかくん <narikakun@gmail.com>
2023-03-16 20:58:24 +0900

    エラー時のJSONを変更

--- routes/board/createBoard.js
+++ routes/board/createBoard.js
@@ -18,7 +18,7 @@ const path = require("path");
 const multerErrorHandler = (err, req, res, next) => {
     if (err) {
         res.status(500).json({
-            message: err.message || err,
+            msg: err.message || err,
         });
     } else {
         next();
