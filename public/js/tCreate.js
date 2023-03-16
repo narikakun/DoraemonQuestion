0a1d218b6c0db4f7c458c0257c50d0fbd612ff2c
nrkycn <narikakun@gmail.com>
2023-03-16 11:16:19 +0900

    クラス作成ページの作成

--- public/js/tCreate.js
+++ public/js/tCreate.js
@@ -8,13 +8,24 @@ $(function(){
                 tPassword: $('#adminPassword').val()
             }),
             contentType: 'application/json',
-            dataType: "json",
+            dataType: "json"
         })
             .done(function(data, textStatus, jqXHR){
-                console.log(data);
+                $.cookie(`adminPass_${data.data._id}`, $('#adminPassword').val());
+                window.location.href = `/admin/${data.data._id}`
             })
             .fail(function(jqXHR, textStatus, errorThrown){
-                console.log(jqXHR.responseJSON);
+                $('#errorMsg').text(jqXHR.responseJSON.msg);
             });
+    });
+    $("#adminPassword").keypress(function(e){
+        if(e.which == 13){
+            $("#createClassButton").click();
+        }
+    });
+    $("#classId").keypress(function(e){
+        if(e.which == 13){
+            $("#createClassButton").click();
+        }
     });
 });
