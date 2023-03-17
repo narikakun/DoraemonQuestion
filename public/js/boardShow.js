1458641c89b4a4d845145c5561b385b216a33bdd
なりかくん <narikakun@gmail.com>
2023-03-17 14:49:12 +0900

    pdfの場合、pdfの画像を表示

--- public/js/boardShow.js
+++ public/js/boardShow.js
@@ -14,9 +14,14 @@ function getBoard (classId, boardId) {
             if (data.data.data.files) {
                 imgResult = await getImg(classId, boardId);
             }
-            for (const img of imgResult.files) {
+            for (const imgKey in imgResult.files) {
+                let img = imgResult.files[imgKey];
+                let showModalJs = `showModal(['${img.url}'])`;
+                if (img.isPdf) {
+                    showModalJs = `showPdf('${classId}', '${boardId}', '${imgKey}')`;
+                }
                 imgHtml += `<div class="col">
-                        <div class="card shadow-sm card-link" data-bs-toggle="modal" data-bs-target="#lightboxModalFullscreen" data-bs-lightbox="${img.url}">
+                        <div class="card shadow-sm card-link" data-bs-toggle="modal" data-bs-target="#lightboxModal" onclick="${showModalJs}">
                             <img src="${img.url}" class="bd-placeholder-img card-img-top">
                             <div class="card-body">
                                 <p class="card-text">${img.name}</p>
@@ -36,6 +41,31 @@ async function getImg(classId, boardId) {
     return new Promise((resolve, reject) => {
             $.ajax({
                 url: `/api/board/${classId}/image/${boardId}`,
+                type: "GET",
+                async: true,
+            }).then(
+                function (result) {
+                    resolve(result);
+                },
+                function () {
+                    reject();
+                }
+            )
+        }
+    )
+}
+
+async function showPdf (classId, boardId, key) {
+    $("#loading-overlay").fadeIn(300);
+    let pdfImages = await getPdf(classId, boardId, key);
+    showModal(pdfImages.files.map(f => f.url));
+    $("#loading-overlay").fadeOut(300);
+}
+
+async function getPdf(classId, boardId, key) {
+    return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `/api/board/${classId}/pdf/${boardId}/${key}`,
                 type: "GET",
                 async: true,
             }).then(
