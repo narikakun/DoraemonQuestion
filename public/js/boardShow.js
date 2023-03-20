768245601d9537d0c1ba67230d5c644e4b1bcf93
なりかくん <narikakun@gmail.com>
2023-03-20 19:54:10 +0900

    コメントに対応

--- public/js/boardShow.js
+++ public/js/boardShow.js
@@ -1,4 +1,4 @@
-function getBoard (classId, boardId) {
+$(function() {
     $.ajax({
         type: "GET",
         url: `/api/board/${classId}/board/${boardId}`,
@@ -42,8 +42,9 @@ function getBoard (classId, boardId) {
             }
             $("#imgList").html(imgHtml);
             if ($.cookie(`adminPass_${classId}`) || data.data.author == $.cookie('username')) {
-                $("#replyBox").html(`<div class="card">
+                $("#replyBox").html(`<div class="card mb-3">
                     <div class="card-body">
+                        <p id="errorMsg2" style="color: red;"></p>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">内容</label>
                             <textarea class="form-control" id="contentInput" rows="3"></textarea>
@@ -56,17 +57,111 @@ function getBoard (classId, boardId) {
                             </div>
                             <div id="fileHelp" class="form-text">png, jpg, pdfが利用できます。ファイルは5MBまでアップロードできます。</div>
                         </div>
-                        <button type="button" class="btn btn-primary">ボードの中に投稿する</button>
+                        <button type="button" class="btn btn-primary" id="postCommentButton">ボードの中に投稿する</button>
                     </div>
                 </div>`);
+                $("#postCommentButton").click(function (event) {
+                    $('#errorMsg2').text("");
+                    $("#loading-overlay").fadeIn(300);
+                    let fd = new FormData();
+                    for (let i = 1; i <= 1; i++) {
+                        let file = $("#inputFile0" + i).prop('files')[0];
+                        if (file) {
+                            fd.append("files", file);
+                        }
+                    }
+                    let content = $("#contentInput").val();
+                    if (content) {
+                        fd.append("content", content);
+                    }
+                    $.ajax({
+                        type: "POST",
+                        url: `/api/comment/${boardId}/post`,
+                        data: fd,
+                        dataType: "json",
+                        cache: false,
+                        processData: false,
+                        contentType: false,
+                    })
+                        .done(function (data, textStatus, jqXHR) {
+                            $("#loading-overlay").fadeOut(300);
+                            console.log(data);
+                            $("#contentInput").val("");
+                            $("#inputFile01").val("");
+                        })
+                        .fail(function (jqXHR, textStatus, errorThrown) {
+                            $('#errorMsg2').text(jqXHR.responseJSON.msg);
+                            $("#loading-overlay").fadeOut(300);
+                        });
+                });
+
+                $("#removeInput01").click(function (e) {
+                    $("#inputFile01").val("");
+                });
             }
+            $.ajax({
+                type: "GET",
+                url: `/api/comment/${boardId}/list`,
+                contentType: 'application/json',
+                dataType: "json"
+            })
+                .done(async function(data, textStatus, jqXHR){
+                    console.log(data);
+                    let replyBoxHtml = "";
+                    for (const comment of data.comments) {
+                        let imgCmtResult = null;
+                        if (comment.data.files) {
+                            if (comment.data.files[0]) {
+                                imgCmtResult = await getCmtImg(comment._id);
+                            }
+                        }
+                        replyBoxHtml += `
+                        <div class="col" id="comment_${comment._id}">
+                            <div class="card mb-3">
+                                <div class="row g-0">
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${comment.author}</h5>
+                                            <p class="card-text">${comment.data.content || ""}</p>
+                                            <p class="card-text"><small class="text-muted">${new Date(comment.createdAt).toLocaleString("ja")}</small></p>
+                                        </div>
+                                    </div>
+                            `;
+                        if (imgCmtResult) {
+                            for (const fileKey in imgCmtResult.files) {
+                                let file = imgCmtResult.files[fileKey];
+                                let showModalJs = `showModal(['${file.url}'])`;
+                                if (file.isPdf) {
+                                    showModalJs = `showCmtPdf('${comment._id}', '${fileKey}')`;
+                                }
+                                replyBoxHtml += `
+                                    <div class="col-md-4 card-link" data-bs-toggle="modal" data-bs-target="#lightboxModal" onclick="${showModalJs}">
+                                        <div class="card bg-dark text-white">
+                                            <img src="${file.url}" class="bd-placeholder-img card-img-top">
+                                            <div class="card-img-overlay">
+                                                <p class="card-text">${file.name}</p>
+                                            </div>
+                                        </div>
+                                    </div>`;
+                            }
+                        }
+                        replyBoxHtml += `
+                                    </div>
+                                </div>
+                            </div>`;
+                    }
+                    $("#replyList").html(replyBoxHtml);
+                })
+                .fail(function(jqXHR, textStatus, errorThrown){
+                    $('#errorMsg').text(jqXHR.responseJSON.msg);
+                });
         })
-        .fail(function(jqXHR, textStatus, errorThrown){
+        .fail(function(jqXHR, textStatus, errorThrown) {
             $("#loading-overlay").fadeOut(300);
             $('#errorMsg').text(jqXHR.responseJSON.msg);
         });
-}
 
+});
 async function getImg(classId, boardId) {
     return new Promise((resolve, reject) => {
             $.ajax({
@@ -92,10 +187,53 @@ async function showPdf (classId, boardId, key) {
     $("#loading-overlay").fadeOut(300);
 }
 
+async function showCmtPdf (commentId, key) {
+    $("#loading-overlay").fadeIn(300);
+    let pdfImages = await getCmtPdf(commentId, key);
+    showModal(pdfImages.files.map(f => f.url));
+    $("#loading-overlay").fadeOut(300);
+}
+
 async function getPdf(classId, boardId, key) {
     return new Promise((resolve, reject) => {
             $.ajax({
                 url: `/api/board/${classId}/pdf/${boardId}/${key}`,
+                type: "GET",
+                async: true,
+            }).then(
+                function (result) {
+                    resolve(result);
+                },
+                function () {
+                    reject();
+                }
+            )
+        }
+    )
+}
+
+async function getCmtImg(commentId) {
+    return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `/api/comment/${commentId}/image`,
+                type: "GET",
+                async: true,
+            }).then(
+                function (result) {
+                    resolve(result);
+                },
+                function () {
+                    reject();
+                }
+            )
+        }
+    )
+}
+
+async function getCmtPdf(commentId, key) {
+    return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `/api/comment/${commentId}/pdf/${key}`,
                 type: "GET",
                 async: true,
             }).then(
